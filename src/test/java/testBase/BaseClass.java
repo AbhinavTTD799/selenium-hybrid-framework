c7022319f5fa4d71621a5eaedab91802d1eefd4c
Budmewar <abudmewar@deloitte.com>
2026-02-21 16:19:26 +0530

    New Code Changes 2/21/2026

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.WebDriver;
 
 public class BaseClass {
 
-	public WebDriver driver;	
-	
+	public WebDriver driver;
+
 	//Created for generating random string for Unique email
 	public static String randomestring(){
 		String generatedString1 = RandomStringUtils.randomAlphabetic(5);
 		return generatedString1;
 	}
+	
+
 	
 }
--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -2,6 +2,8 @@ package testCases;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,9 +15,13 @@ import testBase.BaseClass;
 
 public class LoginTest extends BaseClass {
 	
+	public Logger logger;
+	
 	@BeforeClass
 	public void login()
-	{
+	{	
+		logger= LogManager.getLogger(this.getClass());
+		logger.debug("************* Launching Browser *****************");
 		System.out.println("This is Login Step");
 		System.setProperty("webdriver.chrome.driver","./Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
@@ -28,6 +34,7 @@ public class LoginTest extends BaseClass {
 	@Test
 	public void openapp()
 	{
+		logger.debug("************* Launching Application *****************");
 		System.out.println("This is Open Application Step");
 		LoginPage lp = new LoginPage(driver);
 		lp.setUserName("Admin");
@@ -38,6 +45,7 @@ public class LoginTest extends BaseClass {
 	@AfterClass
 	public void logout()
 	{
+		logger.debug("************* Closing Browser *****************");
 		System.out.println("This is Logout Step");
 		driver.quit();
 	}
